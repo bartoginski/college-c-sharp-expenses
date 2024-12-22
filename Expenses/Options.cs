55ef89846575670fc6000053483cdb059c4e45f5
Bartosz Oginski <bartoginski@outlook.com>
2024-12-22 19:18:10 +0100

    feat: add menu options static class

---
+++ Expenses/Options.cs
@@ -0,0 +1,10 @@
+namespace Expenses
+{
+    static class Options
+    {
+        public static List<String> menuOptions = [
+            "1. Add new expenses",
+            "2. Stats",
+        ];
+    }
+}
