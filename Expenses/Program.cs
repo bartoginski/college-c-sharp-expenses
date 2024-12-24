f9df5c87b43b34175d168ac8d05a3dbf8f2013c8
bartoginski <bartoginski@outlook.com>
2024-12-24 01:01:36 +0100

    feat(program): add edit, remove options + minor changes

--- Expenses/Program.cs
+++ Expenses/Program.cs
@@ -2,110 +2,149 @@
 {
     public static class Program
     {
-        static List<Expense> expenses = [];
-        static String amountCurrency = "zl";
+        private static List<Expense> _expenses =
+        [
+            new Expense("Groceries", 100, "24.12.2024", "Food"),
+            new Expense("Power Bill", 623, "23.12.2024", "Bills"),
+            new Expense("Xbox Game Pass", 67, "24.12.2024", "Entertainment"),
+        ];
+
+        private static string _amountCurrency = "zl";
 
         public static void Main(string[] args)
         {
-            int selectedOption = 0;
+            Console.WriteLine("Welcome to Expenses");
 
+            // Main loop, we will keep showing the menu until the user decides to exit
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Welcome to Expenses");
-                Console.WriteLine("Select an option (using arrows, enter to select):");
+                int selectedOption = Helpers.GetInteractiveMenu(Helpers.MenuOptions, "Main menu");
 
-                for (var i = 0; i < Options.menuOptions.Count; i++)
-                {
-                    var menuOption = Options.menuOptions[i];
-                    if (selectedOption == i)
-                    {
-                        Console.Write("> ");
-                    }
-
-                    Console.WriteLine(menuOption);
-                }
-
-                var userInteraction = Console.ReadKey().Key;
-
-                switch (userInteraction)
-                {
-                    case ConsoleKey.DownArrow:
-                        if (Options.menuOptions.Count - 1 > selectedOption) selectedOption++;
-                        break;
-                    case ConsoleKey.UpArrow:
-                        if (selectedOption > 0) selectedOption--;
-                        break;
-                    case ConsoleKey.Enter:
-                        HandleMenuSelection(selectedOption);
-                        break;
-                }
+                HandleMenuSelection(selectedOption);
             }
         }
-        
+
         private static void HandleMenuSelection(int selectedOption)
         {
             Console.Clear();
 
+            // user choice - 1 
             switch (selectedOption)
             {
-                case 0:
-                    AddNewExpense();
+                case -1:
                     break;
-                case 1:
+                case 0:
                     ShowStats();
                     break;
+                case 1:
+                    Console.WriteLine("Add new expense");
+                    _expenses.Add(GetNewExpense());
+                    break;
                 case 2:
+                    RemoveExpense();
+                    break;
+                case 3:
+                    EditExpenseEntry();
+                    break;
+                case 4:
                     Environment.Exit(0);
                     break;
             }
         }
-        
-        private static void AddNewExpense()
+
+        private static Expense GetNewExpense()
         {
-            Console.WriteLine("Add new expense");
-            
             Console.WriteLine("Enter the title:");
-            var title  = Console.ReadLine() ?? "Unknown";
-            
-            Console.WriteLine($"Enter the amount: ({amountCurrency})");
+            var title = Console.ReadLine()?.Trim() ?? "Unknown";
+
+            Console.WriteLine($"Enter the amount: ({_amountCurrency})");
             var amount = double.TryParse(Console.ReadLine(), out var result) ? result : 0;
+
+            Console.WriteLine("Enter the date (by default today date in format dd-MM-yyyy):");
+            var date = Console.ReadLine()?.Trim() ?? "";
             
             Console.WriteLine("Enter the category:");
-            var category = Console.ReadLine() ?? "";
+            var category = Console.ReadLine()?.Trim() ?? "Uncategorized";
             
-            Console.WriteLine("Enter the date:");
-            var date = Console.ReadLine() ?? "";
-            
-            expenses.Add(new Expense(title, amount, category, date));
             
             Console.WriteLine("Expense added successfully");
             PressAnyKeyToContinue();
+
+            return new Expense(title, amount, date, category);
         }
-        
+
         private static void ShowStats()
         {
             Console.WriteLine("Stats");
-            Console.WriteLine("Total expenses: " + expenses.Count);
-            Console.WriteLine("Total amount: " + expenses.Sum(expense => expense.Amount) + amountCurrency);
+            Console.WriteLine("Total expenses: " + _expenses.Count);
+            Console.WriteLine("Total amount: " + _expenses.Sum(expense => expense.Amount) + _amountCurrency);
 
-            if (expenses.Count == 0)
+            if (_expenses.Count == 0)
             {
                 Console.WriteLine("No expenses to show");
                 PressAnyKeyToContinue();
                 return;
             }
-            
-            Console.WriteLine("===== Full list of expenses =====");
-            foreach (var expense in expenses)
-            {
 
-                Console.WriteLine(expense.Title + " - " + expense.Amount + " - " + expense.Date + " - " + expense.Tag);
+            Console.WriteLine("===== Full list of expenses =====");
+            foreach (var expense in _expenses)
+            {
+                string expenseSummary = "";
+
+                if (expense.Title != "")
+                {
+                    expenseSummary += expense.Title;
+                }
+
+                expenseSummary += " - " + expense.Amount + _amountCurrency;
+                
+                if (expense.Date != "")
+                {
+                    expenseSummary += " - " + expense.Date;
+                }
+                
+                if (expense.Tag != "")
+                {
+                    expenseSummary += " - " + expense.Tag;
+                }
+
+                Console.WriteLine(expenseSummary);
             }
-            
+
             PressAnyKeyToContinue();
         }
-        
+
+        private static void RemoveExpense()
+        {
+            int indexOfExpense = Helpers.GetInteractiveMenu(_expenses
+                .Select(expense => $"{expense.Title} - {expense.Amount}{_amountCurrency}").ToList(), "Select expense to remove");
+            
+            if (indexOfExpense == -1)
+            {
+                return;
+            }
+            
+            Console.WriteLine($"Expense: {_expenses[indexOfExpense].Title} removed successfully");
+
+            _expenses.RemoveAt(indexOfExpense);
+
+            PressAnyKeyToContinue();
+        }
+
+        private static void EditExpenseEntry()
+        {
+            int indexOfExpense = Helpers.GetInteractiveMenu(_expenses
+                .Select(expense => $"{expense.Title} - {expense.Amount}{_amountCurrency}").ToList(), "Select expense to edit");
+            
+            if (indexOfExpense == -1)
+            {
+                return;
+            }
+            
+            Expense newExpense = GetNewExpense();
+            _expenses[indexOfExpense] = newExpense;
+        }
+
         private static void PressAnyKeyToContinue()
         {
             Console.WriteLine("\nPress any key to continue...");
