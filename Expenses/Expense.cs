0359e44f111ef0911b1d0d71ad8126e686495106
bartoginski <bartoginski@outlook.com>
2024-12-24 01:00:37 +0100

    fix(expenses): remove unused hidden property

--- Expenses/Expense.cs
+++ Expenses/Expense.cs
@@ -7,15 +7,13 @@ namespace Expenses
         public double Amount;
         public string Date;
         public string Tag;
-        public bool IsHidden;
 
-        public Expense(string title, double amount, string date, string tag = "", bool isHidden = false)
+        public Expense(string title, double amount, string date = "", string tag = "")
         {
             Title = title;
             Amount = amount;
-            Date = date;
+            Date = date != "" ? date : DateTime.Now.ToString("dd-MM-yyyy");
             Tag = tag;
-            IsHidden = isHidden;
         }
     }
 }
